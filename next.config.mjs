481caf5c94a5af3bd96a9a2b16990d7c0787f084
Sangdang2203 <sangdang2203@gmail.com>
2024-05-22 12:37:48 +0700

    update  dashboard

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
 	webpack: (config, { isServer }) => {
 		if (!isServer) {
 			config.resolve.fallback.fs = false;
+			config.node = {
+				global: true,
+			};
 		}
 		return config;
 	},
