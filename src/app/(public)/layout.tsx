e86d415dbce5256caa34105eae70e36283ff0ec6
Đặng Văn Sang <122693847+Sangdang2203@users.noreply.github.com>
2024-11-12 16:23:13 +0700

    Update layout.tsx

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -9,39 +9,36 @@ import TheFooter from "@/components/TheFooter";
 import SpeedDial from "@/components/SpeedDial";
 
 export const metadata = {
-	title: {
-		// absolute: "",
-		default: "EcoCloud | Điện toán đám mây",
-		template: "%s | Turbo AI Solution",
-	},
-	description:
-		"More than 10 years of experience, Turbo Solutions specializes in providing leading cloud computing solutions and applications in Vietnam.",
+  title: {
+    // absolute: "",
+    default: "Turbo AI Solution",
+    template: "%s | Turbo AI Solution",
+  },
+  description:
+    "More than 10 years of experience, Turbo Solutions specializes in providing leading cloud computing solutions and applications in Vietnam.",
 };
 
 export default function RootLayout(props: { children: React.ReactNode }) {
-	return (
-		<html lang="en">
-			<body>
-				<AppRouterCacheProvider options={{ enableCssLayer: true }}>
-					<ThemeProvider theme={theme}>
-						<CssBaseline />
-						<header>
-							<TheHeader />
-						</header>
+  return (
+    <html lang="en">
+      <body>
+        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <header>
+              <TheHeader />
+            </header>
 
-						<main className="background-body"> {props.children} </main>
+            <main className="background-body"> {props.children} </main>
 
-						<footer>
-							<TheFooter />
-							<SpeedDial />
-						</footer>
-					</ThemeProvider>
-				</AppRouterCacheProvider>
-				<Toaster
-					closeButton
-					richColors
-				/>
-			</body>
-		</html>
-	);
+            <footer>
+              <TheFooter />
+              <SpeedDial />
+            </footer>
+          </ThemeProvider>
+        </AppRouterCacheProvider>
+        <Toaster closeButton richColors />
+      </body>
+    </html>
+  );
 }
