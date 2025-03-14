87d2ed365967d82b2c558e0db40fee0c540c5518
Đặng Văn Sang <122693847+Sangdang2203@users.noreply.github.com>
2025-03-14 20:40:55 +0700

    refactor code

--- src/app/(public)/tin-tuc/[slug]/page.tsx
+++ src/app/(public)/tin-tuc/[slug]/page.tsx
@@ -1,53 +1,52 @@
 "use server";
 
 import * as React from "react";
-import createDOMPurify from "dompurify";
 import { JSDOM } from "jsdom";
-import { fetchPost, fetchPosts } from "@/app/methods/fetchApi";
-import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import createDOMPurify from "dompurify";
 import { Post } from "types/interfaces";
+import { fetchPost, fetchPosts } from "@/app/methods/fetchApi";
 import CustomRating from "@/components/CustomRating";
 import { Box, Container, Typography } from "@mui/material";
 
 export default async function PostDetail({
-	params,
+  params,
 }: {
-	params: { slug: string; a: string };
+  params: { slug: string; a: string };
 }) {
-	const posts: Map<string, Post> = new Map<string, Post>(
-		(await fetchPosts()).map((post: Post) => [post.slug, post])
-	);
+  const posts: Map<string, Post> = new Map<string, Post>(
+    (await fetchPosts()).map((post: Post) => [post.slug, post])
+  );
 
-	const id = posts.get(params.slug)?.id;
+  const id = posts.get(params.slug)?.id;
 
-	let post: Post | null = null;
+  let post: Post | null = null;
 
-	if (id) {
-		post = await fetchPost(id);
-	}
+  if (id) {
+    post = await fetchPost(id);
+  }
 
-	const window = new JSDOM("").window;
-	const DOMPurify = createDOMPurify(window);
+  const window = new JSDOM("").window;
+  const DOMPurify = createDOMPurify(window);
 
-	const sanitizedContent = DOMPurify.sanitize(post?.content || "");
+  const sanitizedContent = DOMPurify.sanitize(post?.content || "");
 
-	return (
-		<Box className="w-full bg-white">
-			<Box className="text-white themePost flex justify-center items-center">
-				<Typography className="text-xl w-3/4 leading-10 font-bold uppercase text-center text-with-shadow">
-					{post?.title}
-				</Typography>
-			</Box>
+  return (
+    <Box className="w-full bg-white">
+      <Box className="text-white themePost flex justify-center items-center">
+        <Typography className="text-xl w-3/4 leading-10 font-bold uppercase text-center text-with-shadow">
+          {post?.title}
+        </Typography>
+      </Box>
 
-			<Container>
-				<Box my={2}>
-					<CustomRating />
-				</Box>
-				<Box
-					className="w-full lg:w-[78%] mx-auto py-5 text-justify"
-					dangerouslySetInnerHTML={{ __html: sanitizedContent }}
-				/>
-			</Container>
-		</Box>
-	);
+      <Container>
+        <Box my={2}>
+          <CustomRating />
+        </Box>
+        <Box
+          className="w-full lg:w-[78%] mx-auto py-5 text-justify"
+          dangerouslySetInnerHTML={{ __html: sanitizedContent }}
+        />
+      </Container>
+    </Box>
+  );
 }
